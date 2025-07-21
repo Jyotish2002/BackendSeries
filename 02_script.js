e635b390c921912a74d588a5f0dba2b636f839c5
Jyotish <jyotishyadav09@gmail.com>
2025-07-22 01:52:42 +0530

    DAY-02

---
+++ 02_script.js
@@ -0,0 +1,42 @@
+const fs=require('fs');
+
+// fs.appendFile('hey.txt'," apna bata kaisa ha",function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("File updated successfully");
+//     }
+// });
+
+// fs.rename('hey.txt','New.txt',(e)=>{
+//     if(e){
+//         console.log(e);
+//     }else{
+//         console.log("File renamed successfully");
+//     }
+// });
+
+// fs.copyFile('New.txt','./hey.txt',(e)=>{
+//     if(e){
+//         console.log(e);
+//     }else{
+//         console.log("File copied successfully");
+//     }
+// }); 
+
+// fs.unlink('New.txt',(e)=>{
+//  if(e){
+//      console.log(e);
+//  }else{
+//      console.log("File deleted successfully");
+//  }
+// });
+
+
+// fs.rmdir('./copy',(e)=>{
+//     if(e){
+//         console.log(e.message);
+//     }else{
+//         console.log("Directory deleted successfully");
+//     }
+// });
