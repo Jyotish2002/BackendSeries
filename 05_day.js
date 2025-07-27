3e5cf1c7594781f25925ab8dd4df281b4375a92d
Jyotish <jyotishyadav09@gmail.com>
2025-07-27 23:34:05 +0530

    done

--- 05_day.js
+++ 05_day.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); // Middleware to parse JSON bodies 
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
